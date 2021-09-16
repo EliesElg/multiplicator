096baa85299694c062fe2dab1dfb7c64c6b5daf8
iessL <81320502+iessL@users.noreply.github.com>
2021-09-17 01:59:28 +0200

    Add files via upload

---
+++ form.php
@@ -0,0 +1,8 @@
+<main class="main">
+    <h2> Choisissez un numéro</h2>
+    <form class="main_form" method="post" action="index.php">
+        <input id="number" name="number" type="number" maxlength="2" autofocus required>
+        <input type="submit" value="GO">
+
+    </form>
+</main>
